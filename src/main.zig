f306c14d63b18acbb8cf6b209b01a5373cfa2d45
Konstantinos <kostas.lampropoulos94@gmail.com>
2025-08-07 02:00:04 +0300

    print tables names found - checkpoint

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,11 @@ const std = @import("std");
 const SQLITE_DEFAULT_PAGE_SIZE: u16 = 4096; // Default page size for SQLite
 const SQLITE_HEADER_SIZE: u16 = 100; // Size of the SQLite header
 const ROOT_CELL_SIZE_OFFSET: u16 = 3; // Offset for the root cell size in the header
+const PAGE_HEADER_TABLE_INTERIOR_SIZE: u16 = 12; // Size of the page header
+const PAGE_HEADER_TABLE_LEAF_SIZE: u16 = 8; // Size of the page header
+const SQLITE_SCHEMA_TYPE_INDEX = 1;
+const SQLITE_SCHEMA_NAME_INDEX = 2;
+const SQLITE_SCHEMA_TYPE_TABLE_NAME_INDEX = 3;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -56,13 +61,162 @@ fn dbInfo(file: std.fs.File) !void {
 }
 
 fn tables(file: std.fs.File) !void {
-    var stdOutWriter = std.io.getStdOut().writer();
-    try stdOutWriter.print("Listing tables is not implemented yet.\n", .{});
+    const allocator = std.heap.page_allocator;
+    // try stdOutWriter.print("Listing tables is being implemented.\n", .{});
     const filePtr = @constCast(&file);
     var parser = Parser.init("CREATE TABLE", filePtr);
     const cellPointers = try parser.parse_cellpointer_array();
     defer cellPointers.deinit();
-    std.debug.print("Cell pointers: size: {d}, page: {d} , items: {}\n", .{ cellPointers.size, cellPointers.pageNo, try cellPointers.get_nth_offset(1) });
+    // std.debug.print("Cell pointers: size: {d}, page: {d} , items[1]: {x}, items: {any}\n", .{ cellPointers.size, cellPointers.pageNo, try cellPointers.get_nth_offset(1), cellPointers.get_cells_pointers() });
+    const table_names = try readPageRecords(filePtr, cellPointers.get_cells_pointers(), TableCellType.Leaf);
+    defer table_names.deinit();
+    var filtered_names = std.ArrayList([]const u8).init(allocator);
+    defer filtered_names.deinit();
+    for (table_names.items) |name| {
+        if (!std.mem.startsWith(u8, name, "sqlite")) {
+            try filtered_names.append(name);
+        }
+    }
+    std.debug.print("Tables found: {s}\n", .{filtered_names.items});
+}
+
+const TableCellType = enum {
+    Interior,
+    Leaf,
+};
+
+fn readPageRecords(file: *std.fs.File, cell_offsets: []u16, cellType: TableCellType) !std.ArrayList([]const u8) {
+    // Implement the logic to read a record from the database file
+    // based on the cell type (Interior or Leaf)
+
+    const allocator = std.heap.page_allocator;
+    var table_names = std.ArrayList([]const u8).init(allocator);
+    for (cell_offsets) |offset| {
+        try file.seekTo(offset);
+        // _ = try file.read(&buf);
+        // const cellSize = std.mem.readInt(u16, &buf, .big);
+        if (cellType == TableCellType.Leaf) {
+            const payload_size = try readVarInt(file);
+            // std.debug.print("Leaf cell varint payload: {d}\n", .{payload_size});
+            const rowId = try readVarInt(file);
+            // std.debug.print("Leaf cell row ID: {d}\n", .{rowId});
+            // Read the rest of the leaf cell data
+            // Read leaf cell data
+            // std.debug.print("Leaf cell data: {s}\n", .{"Payload "});
+            const record_header_size = try readVarInt(file);
+            // std.debug.print("Leaf cell record header size: {d}\n", .{record_header_size});
+            const record_header_field_bytes = varint_byte_count(record_header_size);
+            // std.debug.print("Leaf cell record header field bytes: {d}\n", .{record_header_field_bytes});
+            // const seek_to_record_body_bytes = record_header_size  - record_header_field_bytes;
+            const payload_size_offset = varint_byte_count(payload_size);
+            const row_id_size_bytes = varint_byte_count(rowId);
+
+            const offset_for_record_header = offset + payload_size_offset + row_id_size_bytes + record_header_field_bytes;
+            // std.debug.print("Leaf cell seek to offset: {d}\n", .{offset_for_record_header});
+            try file.seekTo(offset_for_record_header);
+            var headerVarInts = std.ArrayList(u64).init(allocator);
+            defer headerVarInts.deinit();
+            var bytes_read: u16 = 0;
+            while (bytes_read < record_header_size) {
+                const varint = try readVarInt(file);
+                // std.debug.print("Leaf cell header varint: {d}, content size: {d}\n", .{ varint, serialTypeToContentSize(varint) });
+                try headerVarInts.append(serialTypeToContentSize(varint));
+                bytes_read += varint_byte_count(varint);
+            }
+            const offset_for_record_body = offset + payload_size_offset + row_id_size_bytes + record_header_size;
+            // std.debug.print("Leaf cell seek to offset: {d}\n", .{offset_for_record_body});
+            try file.seekTo(offset_for_record_body);
+            var record_body_buf: [256]u8 = undefined; // Adjust size as needed
+            _ = try file.read(record_body_buf[0..payload_size]);
+            record_body_buf = std.mem.zeroes([256]u8);
+            try file.seekTo(offset_for_record_body);
+            const schemaTypeSize = headerVarInts.items[SQLITE_SCHEMA_TYPE_INDEX - 1];
+            _ = try file.read(record_body_buf[0..schemaTypeSize]);
+            const schemaType = try allocator.alloc(u8, schemaTypeSize);
+
+            @memcpy(schemaType, record_body_buf[0..schemaTypeSize]);
+            // std.debug.print("Leaf cell schema type: {s}\n", .{schemaType});
+            record_body_buf = std.mem.zeroes([256]u8);
+            const schemaNameSize = headerVarInts.items[SQLITE_SCHEMA_NAME_INDEX - 1];
+            _ = try file.read(record_body_buf[0..schemaNameSize]);
+            const schemaName = try allocator.alloc(u8, schemaNameSize);
+            @memcpy(schemaName, record_body_buf[0..schemaNameSize]);
+            // std.debug.print("Leaf cell schema name: {s}\n", .{schemaName});
+            record_body_buf = std.mem.zeroes([256]u8);
+            const tableNameSize = headerVarInts.items[SQLITE_SCHEMA_TYPE_TABLE_NAME_INDEX - 1];
+            _ = try file.read(record_body_buf[0..tableNameSize]);
+            const tableName = try allocator.alloc(u8, tableNameSize);
+            @memcpy(tableName, record_body_buf[0..tableNameSize]);
+            // std.debug.print("Leaf cell table name: {s}\n", .{tableName});
+            try table_names.append(tableName);
+        } else {
+            // Read interior cell data
+            std.debug.print("Interior cell data: {any}\n", .{"not done yet"});
+        }
+    }
+    return table_names;
+}
+
+fn serialTypeToContentSize(serial_type: u64) u64 {
+    if (serial_type >= 12 and serial_type % 2 == 0) {
+        return (serial_type - 12) / 2;
+    }
+
+    if (serial_type >= 13 and serial_type % 2 == 1) {
+        return (serial_type - 13) / 2;
+    }
+
+    if (serial_type == 0) {
+        return 0; // NULL
+    } else if (serial_type == 1) {
+        return 1;
+    } else if (serial_type == 2) {
+        return 2;
+    } else if (serial_type == 3) {
+        return 3;
+    } else if (serial_type == 4) {
+        return 4;
+    } else if (serial_type == 5) {
+        return 6;
+    } else if (serial_type == 6) {
+        return 8;
+    } else if (serial_type == 7) {
+        return 8;
+    } else if (serial_type == 8) {
+        return 0; // INT(0)
+    } else if (serial_type == 9) {
+        return 0; // INT(1)
+    } else if (serial_type == 10 or serial_type == 11) {
+        return 0; // variable
+    }
+    return 0;
+}
+
+fn readVarInt(file: *std.fs.File) !u64 {
+    var result: u64 = 0;
+    var shift: u6 = 0;
+    var buf: [1]u8 = undefined;
+
+    while (true) {
+        _ = try file.read(&buf);
+        const byte = buf[0];
+        result |= @as(u64, byte & 0x7F) << shift;
+        if ((byte & 0x80) == 0) break; // MSB not set: last byte
+        shift += 7;
+        if (shift > 63) return error.Overflow; // Prevent overflow
+    }
+
+    // std.debug.print("Read varint: {d}\n", .{result});
+    return result;
+}
+
+fn varint_byte_count(value: u64) u8 {
+    var n: u8 = 1;
+    var v = value;
+    while (v >= 0x80) : (v >>= 7) {
+        n += 1;
+    }
+    return n;
 }
 
 // Define the CellPointerArray structure
@@ -102,8 +256,8 @@ const CellPointerArray = struct {
         return (offset + SQLITE_DEFAULT_PAGE_SIZE - 1) / SQLITE_DEFAULT_PAGE_SIZE;
     }
 
-    pub fn get_cells_pointers(self: *Self) []u16 {
-        return self.cells_pointers;
+    pub fn get_cells_pointers(self: Self) []u16 {
+        return self.cells_pointers.items;
     }
 
     /// Returns the nth cell pointer from the array.
@@ -141,7 +295,7 @@ const Parser = struct {
         var buf: [2]u8 = undefined;
         _ = try self.file.read(&buf);
         const cellsCount = std.mem.readInt(u16, &buf, .big);
-
+        try self.file.seekTo(SQLITE_HEADER_SIZE + PAGE_HEADER_TABLE_LEAF_SIZE);
         for (0..cellsCount) |_| {
             _ = try self.file.read(&buf);
             const cellOffset = std.mem.readInt(u16, &buf, .big);
